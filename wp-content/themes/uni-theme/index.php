1c53c3548f0dd64355ba16cf05ce10a26261d3be
Thai Truong <thaitruong@thais-air.home>
2022-01-06 23:16:11 +0000

    loop

--- wp-content/themes/uni-theme/index.php
+++ wp-content/themes/uni-theme/index.php
@@ -1,1 +1,14 @@
-<?php ?>
+<?php 
+
+$names = array("thai", "bingo", "wings", "cat");
+
+$count = 0;
+
+while ($count < count($names)) {
+echo "<li>Hi, my name is $names[$count]</li>";
+$count++;
+
+}
+
+?>
+
